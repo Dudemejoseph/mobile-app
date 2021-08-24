d50c81ad1677719e4a4c0aa7ffe657ba195222b9
gloverola <glovermartial@gmail.com>
2021-08-24 08:35:42 +0100

    feat: add activities screen

--- src/screens/farm/Activities.js
+++ src/screens/farm/Activities.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Text, View, ScrollView, TouchableOpacity, Image } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
 import { ScaledSheet } from "react-native-size-matters";
 import Wrapper from "../../components/Wrapper";
 import { COLORS } from "../../constants/theme";
@@ -28,6 +35,61 @@ const Activities = ({ navigation }) => {
         </View>
 
         <Text style={styles.headerTxt}>Activities</Text>
+
+        {/* ======= Log In Activities ========= */}
+        <View style={styles.formView}>
+          <Text style={styles.headTxt}>Log In Activities</Text>
+          <View style={styles.form}>
+            {/* ========== Fertilizer Application ========= */}
+            <TouchableOpacity activeOpacity={0.4} style={styles.dropBtn}>
+              <Text style={styles.dropTxt}>Fertilizer Application</Text>
+              <Image
+                source={require("../../assets/icons/drop-icon.png")}
+                style={styles.dropIcon}
+              />
+            </TouchableOpacity>
+
+            {/* ======== Amount ========== */}
+            <TextInput
+              placeholder='N0.00'
+              style={styles.input}
+              placeholderTextColor={COLORS.text_grey}
+            />
+
+            {/* ========== Select Date ========= */}
+            <TouchableOpacity activeOpacity={0.4} style={styles.dateBtn}>
+              <Image
+                source={require("../../assets/icons/calender-icon.png")}
+                style={styles.dateIcon}
+              />
+              <Text style={styles.dropTxt}>Select Date</Text>
+            </TouchableOpacity>
+
+            {/* ========== Time Frame ========= */}
+            <TouchableOpacity activeOpacity={0.4} style={styles.dropBtn}>
+              <Text style={styles.dropTxt}>Time Frame</Text>
+              <Image
+                source={require("../../assets/icons/drop-icon.png")}
+                style={styles.dropIcon}
+              />
+            </TouchableOpacity>
+
+            {/* ======== Description ========== */}
+            <TextInput
+              placeholder='Description...'
+              style={styles.inputDesc}
+              multiline
+              placeholderTextColor={COLORS.text_grey}
+            />
+
+            {/* ========= Buttons ======== */}
+            <View style={styles.btnView}>
+              <TouchableOpacity activeOpacity={0.6} style={styles.createBtn}>
+                <Text style={styles.createTxt}>Create</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </ScrollView>
     </Wrapper>
   );
@@ -63,5 +125,107 @@ const styles = ScaledSheet.create({
     fontWeight: "500",
     fontFamily: "Poppins-Regular",
     marginTop: "20@vs",
+  },
+  formView: {
+    width: "100%",
+    marginTop: "20@ms",
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+  },
+
+  headTxt: {
+    fontWeight: "500",
+    fontSize: "14@ms",
+    fontFamily: "Poppins-Regular",
+    padding: "12@ms",
+    backgroundColor: COLORS.surface,
+    borderTopRightRadius: 8,
+    borderTopLeftRadius: 8,
+  },
+  form: {
+    width: "100%",
+    padding: "14@ms",
+  },
+  input: {
+    width: "100%",
+    height: "40@vs",
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    borderRadius: 4,
+    color: COLORS.text_dark,
+    paddingHorizontal: "10@ms",
+    fontFamily: "Poppins-Regular",
+    marginTop: "15@vs",
+  },
+  inputDesc: {
+    width: "100%",
+    height: "80@vs",
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    borderRadius: 4,
+    color: COLORS.text_dark,
+    paddingHorizontal: "10@ms",
+    fontFamily: "Poppins-Regular",
+    marginTop: "15@vs",
+    backgroundColor: COLORS.surface,
+  },
+  dropBtn: {
+    height: "40@vs",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    borderRadius: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: "10@ms",
+    marginTop: "15@vs",
+  },
+  dateBtn: {
+    height: "40@vs",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    borderRadius: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: "10@ms",
+    marginTop: "15@vs",
+  },
+  dropIcon: {
+    width: "10@ms",
+    height: "10@ms",
+    resizeMode: "contain",
+  },
+  dateIcon: {
+    width: "18@ms",
+    height: "18@ms",
+    resizeMode: "contain",
+    marginRight: "10@ms",
+  },
+  dropTxt: {
+    color: COLORS.text_grey,
+    fontFamily: "Poppins-Regular",
+  },
+  btnView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginTop: "40@vs",
+  },
+  createBtn: {
+    width: "90@ms",
+    height: "35@vs",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.primary,
+    borderRadius: 4,
+  },
+  createTxt: {
+    fontWeight: "500",
+    fontFamily: "Poppins-Regular",
+    color: COLORS.background,
   },
 });
